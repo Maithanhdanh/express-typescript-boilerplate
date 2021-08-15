f082200997c61d661e064934f8e51b117e59f1bd
Dantis <mtd.maithanhdanh@gmail.com>
2021-08-15 21:37:28 +0700

    fix unit test

--- __tests__/unit/config/express.test.ts
+++ __tests__/unit/config/express.test.ts
@@ -1,8 +1,8 @@
 import supertest from 'supertest';
 import { createServer } from '@config/express';
 
-describe('Server', () => {
-  const app = createServer();
+describe('Server', async () => {
+  const app = await createServer();
 
   it('should pass', (done) => {
     supertest(app).get('/health').expect('UP', done);
