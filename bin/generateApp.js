e5baa7ae3a998ab411b4c03dfc1630fef8c7c506
Dantis <mtd.maithanhdanh@gmail.com>
2021-12-11 18:16:50 +0700

    config for publishing npm package

--- bin/generateApp.js
+++ bin/generateApp.js
@@ -1,3 +1,7 @@
+#! /usr/bin/env node
+
+'use strict';
+
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
@@ -14,15 +18,18 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = 'https://github.com/Maithanhdanh/express-typescript-boilerplate.git';
+const git_repo =
+  'https://github.com/Maithanhdanh/express-typescript-boilerplate.git';
 
 try {
-  fs.mkdirSync(appPath);
+  fs.mkdirSync(projectPath);
 } catch (err) {
   if (err.code === 'EEXIST') {
-    console.log('Directory already exists. Please choose another name for the project.');
+    console.log(
+      'Directory already exists. Please choose another name for the project.',
+    );
   } else {
-    console.log(error);
+    console.log(err);
   }
   process.exit(1);
 }
@@ -32,29 +39,27 @@ async function main() {
     console.log('Downloading files...');
     execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
-    
     // Change directory
-    process.chdir(appPath);
-
-    // Install dependencies
-    const useYarn = await hasYarn();
-    console.log('Installing dependencies...');
-    if (useYarn) {
-      await runCmd('yarn install');
-    } else {
-      await runCmd('npm install');
-    }
-    console.log('Dependencies installed successfully.');
-    console.log();
+    process.chdir(projectPath);
 
     console.log('Removing useless files');
+    fs.unlinkSync(path.join(projectPath, 'SECURITY.md'));
+    fs.unlinkSync(
+      path.join(projectPath, '.github/workflows/codeql-analysis.yml'),
+    );
+    fs.unlinkSync(path.join(projectPath, 'bin', 'generateApp.js'));
+    fs.rmdirSync(path.join(projectPath, 'bin'));
     execSync('npx rimraf ./.git');
-    fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
 
-    console.log('The installation is done, this is ready to use !');
-
+    console.log('Initializing project successfully!!!');
+    console.log();
+    console.log('DantisMai =>> Thank you very much');
   } catch (error) {
     console.log(error);
   }
+  console.log();
+  console.log(
+    'Any comments or questions are welcome!, mtd.maithanhdanh@gmail.com',
+  );
 }
 main();
